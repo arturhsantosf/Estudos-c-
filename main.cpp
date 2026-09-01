876fd591ab676765fe15d8e9bb567bb7eae7d0e5
arturhsantosf <artur.h.santos.f@gmail.com>
2026-09-01 17:17:17 -0300

    Alteração 1

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout <<"********************************************"<<endl;
+    cout <<"*Ola mundo,bem vindo ao jogo da adivinhação*"<<endl;
+    cout <<"********************************************"<<endl;
+}
